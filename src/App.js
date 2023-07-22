bfd35345cf0525ad513dae29f8d2920b04843a6e
Denys Bazyliak <franciskdelyamon@gmail.com>
2023-07-22 13:28:46 +0200

    3rd - refactor code & give it flexibility

--- src/App.js
+++ src/App.js
@@ -11,15 +11,29 @@ function App() {
         1: "Step 1: Learn React ",
         2: "Step 2: Fuck  React ",
         3: "Step 3: Be useful ",
-        4: "Step 3: suck dick Viktor ",
+        4: "Step 4: suck dick Viktor ",
     }
-    function setNumberOfState(number){
-        return setStep(step +number)
+
+    function setPrevious(){
+        console.log("this is previous")
+       if(step>1) {
+           return setStep(step - 1)
+       } else{
+           console.log("steps can't be less than 1")
+       }
+    }
+    function setNext(){
+        console.log("this is next")
+        if(step<Object.keys(steps).length) {
+            return setStep(step + 1)
+        } else{
+            console.log(`steps can't be more than ${steps.length}`)
+        }
     }
 
     const numbers = Object.keys(steps).map((number) => {
         return (
-            <div className={step >= Number(number) ? "number selected" : "number"}>{number}</div>
+            <div key={number} className={step >= Number(number) ? "number selected" : "number"}>{number}</div>
         )
     })
     return (
@@ -34,8 +48,8 @@ function App() {
                 {steps[`${step}`]}<BiLogoReact className={"reactIcon"}/>
             </p>
             <div className={"buttons"}>
-                <button className={"button"} onClick={()=>setNumberOfState(-1)}>Previous</button>
-                <button className={"button"} onClick={() => setNumberOfState( 1)}>Next</button>
+                <button className={"button"} onClick={()=>setPrevious()}>Previous</button>
+                <button className={"button"} onClick={()=>setNext()}>Next</button>
             </div>
         </div>
     );
